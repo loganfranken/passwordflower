aa63ab390193a3a941a153c02dcbad3e284822eb
Logan Franken <loganfranken@gmail.com>
2018-06-22 10:43:28 -0700

    Updated implementation to analyze password on submission

--- script/main.js
+++ script/main.js
@@ -1,54 +1,70 @@
+var passwordForm = document.getElementById('password-form');
 var passwordInput = document.getElementById('password-input');
 var resultsOutput = document.getElementById('results-output');
 
-passwordInput.addEventListener('keyup', function() {
-  var results = analyzePassword(passwordInput.value);
-  outputResults(results);
+passwordForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  var resultsSummary = analyzePassword(passwordInput.value);
+  outputResults(resultsSummary);
 });
 
 function analyzePassword(input)
 {
-  var results = [];
+  var summary = {};
+
+  summary.results = [];
+  var maxStrengthPoints = 5;
+  var strengthPoints = maxStrengthPoints;
 
   // Minimum length
   if(input.length < 8)
   {
-    results.push('Password must contain a minimum of 8 characters')
+    summary.results.push('Password must contain a minimum of 8 characters');
+    strengthPoints--;
   }
 
   // Contain an uppercase character
   if(!/[A-Z]/g.test(input))
   {
-    results.push('Password must contain an uppercase character')
+    summary.results.push('Password must contain an uppercase character');
+    strengthPoints--;
   }
 
   // Contain a lowercase character
   if(!/[a-z]/g.test(input))
   {
-    results.push('Password must contain a lowercase character')
+    summary.results.push('Password must contain a lowercase character');
+    strengthPoints--;
   }
 
   // Contain a digit
   if(!/[0-9]/g.test(input))
   {
-    results.push('Password must contain a digit')
+    summary.results.push('Password must contain a digit');
+    strengthPoints--;
   }
 
   // Contain a special character
   if(!/[\W]/g.test(input))
   {
-    results.push('Password must contain a special character')
+    summary.results.push('Password must contain a special character');
+    strengthPoints--;
   }
 
-  return results;
+  summary.passwordStrength = (strengthPoints/maxStrengthPoints) * 100;
+  return summary;
 }
 
-function outputResults(results)
+function outputResults(resultsSummary)
 {
   var resultsHtml = '';
 
+  // Password Strength
+  resultsHtml += '<div><strong>Password Strength:</strong> ' + resultsSummary.passwordStrength + '</div>';
+
+  // Results
   resultsHtml += '<ul>';
-  results.forEach(function(result) {
+  resultsSummary.results.forEach(function(result) {
     resultsHtml += '<li>' + result + '</li>';
   });
   resultsHtml += '</ul>';
