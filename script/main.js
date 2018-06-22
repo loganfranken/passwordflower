e2e09941ee12c19fa7f81ac30d440bf57c698737
Logan Franken <loganfranken@gmail.com>
2018-06-22 09:43:55 -0700

    Created a simple prototype for assessing password complexity

--- script/main.js
+++ script/main.js
@@ -1,1 +1,57 @@
-console.log('Test!');
+var passwordInput = document.getElementById('password-input');
+var resultsOutput = document.getElementById('results-output');
+
+passwordInput.addEventListener('keyup', function() {
+  var results = analyzePassword(passwordInput.value);
+  outputResults(results);
+});
+
+function analyzePassword(input)
+{
+  var results = [];
+
+  // Minimum length
+  if(input.length < 8)
+  {
+    results.push('Password must contain a minimum of 8 characters')
+  }
+
+  // Contain an uppercase character
+  if(!/[A-Z]/g.test(input))
+  {
+    results.push('Password must contain an uppercase character')
+  }
+
+  // Contain a lowercase character
+  if(!/[a-z]/g.test(input))
+  {
+    results.push('Password must contain a lowercase character')
+  }
+
+  // Contain a digit
+  if(!/[0-9]/g.test(input))
+  {
+    results.push('Password must contain a digit')
+  }
+
+  // Contain a special character
+  if(!/[\W]/g.test(input))
+  {
+    results.push('Password must contain a special character')
+  }
+
+  return results;
+}
+
+function outputResults(results)
+{
+  var resultsHtml = '';
+
+  resultsHtml += '<ul>';
+  results.forEach(function(result) {
+    resultsHtml += '<li>' + result + '</li>';
+  });
+  resultsHtml += '</ul>';
+
+  resultsOutput.innerHTML = resultsHtml;
+}
